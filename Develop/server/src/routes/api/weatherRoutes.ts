d1c947535afdcce5f0b501621ab23f1ff1f053bd
jaegerthomas <bjaegerthomas@gmail.com>
2025-01-08 18:24:02 -0500

    updated weatherRoutes.ts reconfigure of post request for city name

--- Develop/server/src/routes/api/weatherRoutes.ts
+++ Develop/server/src/routes/api/weatherRoutes.ts
@@ -6,34 +6,24 @@ import HistoryService from '../../service/historyService.js';
 // import WeatherService from '../../service/weatherService.js';
 import WeatherService from '../../service/weatherService.js';
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
 
-});
-  // TODO: GET weather data from city name
-router.get('/', async (req: Request, res: Response) => {
   const { city } = req.body;
   if (!city) {
     return res.status(400).json({ error: 'City name is required' });
   }
 
   try {
+  // TODO: GET weather data from city name
     const weatherData = await WeatherService.getCities(city);
     res.json(weatherData);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to retrieve weather data' });
-  }
-});
   // TODO: save city to search history
-router.update('/history').post(async (req: Request, res: Response) => {
-  const { city } = req.body;
-  try {
     const newCity = await HistoryService.addCity(city);
     res.json(newCity);
   } catch (error) {
     res.status(500).json({ error: 'Failed to retrieve weather data' });
   }
 });
-
 
 // TODO: GET search history
 router.get('/history', async (req: Request, res: Response) => {
