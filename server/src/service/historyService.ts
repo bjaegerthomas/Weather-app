4cf15872c74e9210c6f2f5a082b863c2ee0fc40b
jaegerthomas <bjaegerthomas@gmail.com>
2025-03-28 09:57:51 -0400

    historyService missing } and realigned for readability

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -20,12 +20,13 @@ class HistoryService {
       encoding: 'utf8',
     });
   }
+
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   private async write(cities: City[]) {
     return await fs.writeFile('db/db.json', JSON.stringify(cities, null, '\t'));
   }
-  // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-   async getCities() {
+// TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
+  async getCities() {
     return await this.read().then((cities) => {
       let parsedCities: City[];
 
@@ -38,25 +39,27 @@ class HistoryService {
 
       return parsedCities;
     });
-   }
+  }
+
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   async addCity(city: string) {
-    if (!city) {
-      throw new Error('City cannot be blank');
-    }
-
-    const newCity: City = { name: city, id: uuidv4() };
-
-    return await this.getCities()
-      .then((cities) => {
-        if (cities.find((index) => index.name === city)) {
-          return cities;
-        }
-        return [...cities, newCity];
-      })
-      .then((updatedCities) => this.write(updatedCities))
-      .then(() => newCity);
+  if (!city) {
+    throw new Error('City cannot be blank');
   }
+
+  const newCity: City = { name: city, id: uuidv4() };
+
+  return await this.getCities()
+    .then((cities) => {
+      if (cities.find((index) => index.name === city)) {
+        return cities;
+      }
+      return [...cities, newCity];
+    })
+    .then((updatedCities) => this.write(updatedCities))
+    .then(() => newCity);
+  }
+}
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
  
 
